8beffe1a4b3108798b0a051fdc72077f8705bbdf
AndiswaRadebe <radebealicia140@gmail.com>
2022-12-13 06:11:08 -0800

    6-main.c

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = (-1);
+    printf("%d\n", r);
+    r = (0);
+    printf("%d\n", r);
+    r = (1);
+    printf("%d\n", r);
+    r = (-98);
+    printf("%d\n", r);
+    return (0);
+}
